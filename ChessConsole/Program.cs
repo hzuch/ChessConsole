620ba02632eef271d1a441c1f6e53c9284e118dc
Heldz <helder.zuchinalli@gmail.com>
2021-07-07 19:52:27 +0100

    Creates class Position

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Board;
 
 namespace ChessConsole
 {
@@ -6,7 +7,11 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position P;
+
+            P = new Position(3, 4);
+
+            Console.WriteLine(P.ToString());
         }
     }
 }
