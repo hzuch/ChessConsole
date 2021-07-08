8cc2f87ee05da1e8e735ace6306e67c85e1ecb5a
Heldz <helder.zuchinalli@gmail.com>
2021-07-08 19:05:18 +0100

    Creates Class ChessPosition to convert a eg. A1 or D6 chess position to 2d array position

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -8,20 +8,9 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
-
-                board.addPiece(new Rook(board, Colour.Black), new Position(0, 0));
-                board.addPiece(new Rook(board, Colour.White), new Position(1, 3));
-                board.addPiece(new King(board, Colour.Black), new Position(2, 4));
-
-                Screen.printBoard(board);
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PositionChess pos = new PositionChess('c', 7);
+            Console.WriteLine(pos.ToString());
+            Console.WriteLine(pos.toPosition());
             
         }
     }
